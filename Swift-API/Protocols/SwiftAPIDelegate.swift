02152a40c1ac4e6664c2420da028c1427e93235c
Marek Kojder <marek.kojder@xsolve.pl>
2016-12-13 13:29:47 +0100

    Added license file and example file.

---
+++ Swift-API/Protocols/SwiftAPIDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  SwiftAPIDelegate.swift
+//  Swift-API
+//
+//  Created by Marek Kojder on 13.12.2016.
+//  Copyright © 2016 XSolve. All rights reserved.
+//
+
+import UIKit
+
+class SwiftAPIDelegate: NSObject {
+
+}
