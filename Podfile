57715e6444ae3c7231e178ae7d6f8fb3a04493e5
Marek Kojder <marek.kojder@xsolve.pl>
2016-12-13 12:54:12 +0100

    Initial commit

---
+++ Podfile
@@ -0,0 +1,13 @@
+platform :ios, '8.0'
+
+target 'Swift-API' do
+  use_frameworks!
+  # Pods for Swift-API
+
+  target 'Swift-APITests' do
+    inherit! :search_paths
+    # Pods for testing
+
+  end
+
+end
---
+++ Swift-API/Swift-API.h
@@ -0,0 +1,20 @@
+//
+//  Swift-API.h
+//  Swift-API
+//
+//  Created by Marek Kojder on 13.12.2016.
+//  Copyright © 2016 XSolve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+//! Project version number for Swift-API.
+FOUNDATION_EXPORT double Swift_APIVersionNumber;
+
+//! Project version string for Swift-API.
+FOUNDATION_EXPORT const unsigned char Swift_APIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swift_API/PublicHeader.h>
+
+
