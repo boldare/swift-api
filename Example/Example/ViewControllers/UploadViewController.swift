f2f863eb491a74f95900022a96f013bcf802492d
Marek Kojder <marek.kojder@xsolve.pl>
2017-02-01 12:49:27 +0100

    Added example of uploading file with progress.

--- Example/Example/ViewControllers/UploadViewController.swift
+++ Example/Example/ViewControllers/UploadViewController.swift
@@ -11,34 +11,140 @@ import SwiftAPI
 
 class UploadViewController: UIViewController {
 
+    @IBOutlet var largeImageSwitch: UISwitch!
+    @IBOutlet var backgroundSwitch: UISwitch!
+    @IBOutlet var textView: UITextView!
+    @IBOutlet var progressBar: UIProgressView!
+
+    fileprivate var progress = Progress(totalUnitCount: 0)
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        textView.text = ""
+        progressBar.progress = 0.0
+        progress.addObserver(self, forKeyPath: "fractionCompleted", options: .new, context: nil)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        progress.removeObserver(self, forKeyPath: "fractionCompleted")
+    }
+
+    func prepareForRequest() {
+        textView.text = ""
+    }
+
+    func startProgress(with request: ApiRequest) {
+        if let p = request.progress {
+            progress.totalUnitCount += 1
+            progress.addChild(p, withPendingUnitCount: 1)
+        }
+        progressBar.progress = Float(progress.fractionCompleted)
+    }
+
+    func reserProgress() {
+        progress.removeObserver(self, forKeyPath: "fractionCompleted")
+        progress = Progress(totalUnitCount: 0)
+        progress.addObserver(self, forKeyPath: "fractionCompleted", options: .new, context: nil)
+    }
+
+    @IBAction func postRequestButtonDidPush() {
+        let destinationUrl = apiRootURL.appendingPathComponent("post")
+
+        prepareForRequest()
+        let request = apiService.postFile(from: fileToUpload, to: destinationUrl, inBackground: backgroundSwitch.isOn, completionHandler: completionHandler)
+        startProgress(with: request)
+    }
+
+    @IBAction func putRequestButtonDidPush() {
+        let destinationUrl = apiRootURL.appendingPathComponent("put")
+
+        prepareForRequest()
+        let request = apiService.putFile(from: fileToUpload, to: destinationUrl, inBackground: backgroundSwitch.isOn, completionHandler: completionHandler)
+        startProgress(with: request)
+    }
+
+    @IBAction func patchRequestButtonDidPush() {
+        let destinationUrl = apiRootURL.appendingPathComponent("patch")
+
+        prepareForRequest()
+        let request = apiService.patchFile(from: fileToUpload, to: destinationUrl, inBackground: backgroundSwitch.isOn, completionHandler: completionHandler)
+        startProgress(with: request)
+    }
+
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "fractionCompleted", let p = object as? Progress {
+            DispatchQueue.main.async {
+                self.progressBar.progress = Float(p.fractionCompleted)
+            }
+        }
     }
 }
 
-extension UploadViewController {
+fileprivate extension UploadViewController {
 
-    fileprivate var apiService: ApiService {
+    var apiService: ApiService {
         return (UIApplication.shared.delegate as! AppDelegate).apiService
     }
 
-    fileprivate var documentsUrl: URL {
-        return URL(fileURLWithPath: NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0], isDirectory: true)
+    var apiRootURL: URL {
+        return URL(string: "https://httpbin.org")!
     }
 
-    fileprivate var smallRemoteFileUrl: URL {
-        return URL(string: "https://upload.wikimedia.org/wikipedia/commons/d/d1/Mount_Everest_as_seen_from_Drukair2_PLW_edit.jpg")!
+    var smallLocalFileURL: URL {
+        return Bundle(for: type(of: self)).url(forResource: "smallImage", withExtension: "jpg")!
     }
 
-    fileprivate var bigRemoteFileUrl: URL {
-        return URL(string: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Fronalpstock_big.jpg")!
-    }
-
-    fileprivate var smallLocalFileURL: URL {
-        return Bundle(for: type(of: self)).url(forResource: "smallImage", withExtension: "png")!
-    }
-
-    fileprivate var bigLocalFileURL: URL {
+    var bigLocalFileURL: URL {
         return Bundle(for: type(of: self)).url(forResource: "bigImage", withExtension: "jpg")!
+    }
+
+    var fileToUpload: URL {
+        if largeImageSwitch.isOn {
+            return bigLocalFileURL
+        } else {
+            return smallLocalFileURL
+        }
+    }
+
+    var completionHandler: ApiResponseCompletionHandler {
+        return {[weak self] (response: ApiResponse?, error: Error?) in
+            guard let strongSelf = self else {
+                return
+            }
+            if strongSelf.progress.completedUnitCount == strongSelf.progress.totalUnitCount {
+                strongSelf.reserProgress()
+            }
+            if let error = error {
+                DispatchQueue.main.async {
+                    strongSelf.textView.text = "Error ocured during request:\n\(error.localizedDescription)"
+                }
+            } else if let response = response {
+                var readable = ""
+                if let url = response.url {
+                    readable.append("URL: \(url)\n")
+                }
+                readable.append("Status code: \(response.statusCode.rawValue) \(response.statusCode.description)\n")
+                if let mime = response.mimeType {
+                    readable.append("MIME Type: \(mime)\n")
+                }
+                if let headers = response.allHeaderFields {
+                    readable.append("Headers:\n")
+                    for header in headers {
+                        readable.append("   \(header.key) : \(header.value)\n")
+                    }
+                }
+                if let body = response.body, let json = String(data: body, encoding: .utf8){
+                    readable.append("Body:\n")
+                    readable.append(json)
+                }
+                DispatchQueue.main.async {
+                    strongSelf.textView.text = readable
+                    strongSelf.textView.setContentOffset(.zero, animated: false)
+                }
+            }
+        }
     }
 }
