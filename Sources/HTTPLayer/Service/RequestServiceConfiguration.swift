732db9df211cf17f070e0ac16fff06c8c8ee08d9
Marek Kojder <marek.kojder@xsolve.pl>
2017-07-18 10:38:52 +0200

    Changed request service configuration to struct.

--- Sources/HTTPLayer/Service/RequestServiceConfiguration.swift
+++ Sources/HTTPLayer/Service/RequestServiceConfiguration.swift
@@ -8,33 +8,57 @@
 
 import Foundation
 
-/**
- Available session configurations using while sending requests.
+///Available session configurations using while sending requests.
+struct RequestServiceConfiguration {
 
- - foreground: indicates sending request only when app is running.
- - background: indicates sending request also when app is not running or when is terminated by system.
- */
-enum RequestServiceConfiguration {
-    case foreground
-    case background
+    ///Indicates sending request only when app is running.
+    static var foreground: RequestServiceConfiguration {
+        return RequestServiceConfiguration(urlSessionConfiguration: .default)
+    }
+
+    ///Indicates sending request also when app is not running or when is terminated by system.
+    static var background: RequestServiceConfiguration {
+        var identifier: String
+        if let name = Bundle.main.infoDictionary?[kCFBundleNameKey as String] as? String,
+            let version = Bundle.main.infoDictionary?[kCFBundleVersionKey as String] as? String {
+            identifier = "\(name)-\(version)"
+        } else {
+            identifier = "SwiftAPIBackgroundSession"
+        }
+        return RequestServiceConfiguration(urlSessionConfiguration: .background(withIdentifier: identifier))
+    }
+
+    ///*URLSessionConfiguration* object for current session.
+    private(set) var urlSessionConfiguration: URLSessionConfiguration
+
+    ///Initializer without parameters is not allowed for this struct.
+    private init() {
+        self.urlSessionConfiguration = .default
+    }
+
+    ///Initializing with configuration.
+    private init(urlSessionConfiguration: URLSessionConfiguration) {
+        self.urlSessionConfiguration = urlSessionConfiguration
+    }
+
+    ///Initializing with custom settings
+    init(cellularAccess: Bool = true, requestTimeout: TimeInterval = 60, useCookies: Bool = true) {
+        let config = URLSessionConfiguration.default
+        config.allowsCellularAccess = cellularAccess
+        config.timeoutIntervalForRequest = requestTimeout
+        config.httpShouldSetCookies = useCookies
+
+        self.urlSessionConfiguration = config
+    }
 }
 
-extension RequestServiceConfiguration {
+extension RequestServiceConfiguration: Hashable {
 
-    ///*URLSessionConfiguration* object for current session
-    var urlSessionConfiguration: URLSessionConfiguration {
-        switch self {
-        case .foreground:
-            return URLSessionConfiguration.default
-        case .background:
-            var identifier: String
-            if let name = Bundle.main.infoDictionary?[kCFBundleNameKey as String] as? String,
-                let version = Bundle.main.infoDictionary?[kCFBundleVersionKey as String] as? String {
-                identifier = "\(name)-\(version)"
-            } else {
-                identifier = "SwiftAPIBackgroundSession"
-            }
-            return URLSessionConfiguration.background(withIdentifier: identifier)
-        }
+    public var hashValue: Int {
+        return urlSessionConfiguration.hashValue
+    }
+
+    public static func ==(lhs: RequestServiceConfiguration, rhs: RequestServiceConfiguration) -> Bool {
+        return lhs.urlSessionConfiguration == rhs.urlSessionConfiguration
     }
 }
